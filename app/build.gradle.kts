933a5cae81de44a04c2b3307ae1ee2866df3cf20
lol91011 <76667998+lol91011@users.noreply.github.com>
2026-07-30 13:11:44 +0300

    Remove debug signingConfigs from build.gradle.kts
    Removed debug signing configuration from build.gradle.kts.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,15 +19,6 @@ android {
         }
     }
 
-    signingConfigs {
-        getByName("debug") {
-            storeFile = file("debug.keystore")
-            storePassword = "android"
-            keyAlias = "androiddebugkey"
-            keyPassword = "android"
-        }
-    }
-
     buildTypes {
         release {
             isMinifyEnabled = false
